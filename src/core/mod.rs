fcee11378bb1334b2f4e67d539cf788d3d07e029
Michael Bednarek <mbednarek360@protonmail.com>
2020-03-04 17:49:45 -0500

    WIP CSV interaction

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,6 +1,28 @@
 pub mod log;
+use csv::Reader;
+use std::collections::HashMap;
+
+fn read_csv(file_name: &String) -> HashMap<String, u64> {
+    let mut outp = HashMap::new();
+    let mut records = Reader::from_path(file_name).unwrap();
+    for service in records.records() {
+        outp.insert(
+            (*((&service).as_ref().unwrap().get(0).unwrap())).to_string(),
+            service.unwrap().get(1).unwrap().to_string().parse::<u64>().unwrap());
+    }
+    outp
+}
 
 pub fn add_service(service: &String, file_name: &String) {
+
+    // read file
+    println!("{:?}", read_csv(file_name));
+
+    // error if duplicate
+
+    // check ids and create unique
+
+    // write to file
 
 }
 
